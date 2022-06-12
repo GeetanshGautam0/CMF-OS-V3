02ac9a2903828f30896fde411dc0855acf637a42
GeetanshGautam0 <loveplanes@hotmail.com>
2022-06-12 15:35:56 -0700

    Added support for font
    Added putchar + pustr functions
    Added CursorPos implementation

--- cmfOS/gnu-efi/bootloader/main.c
+++ cmfOS/gnu-efi/bootloader/main.c
@@ -12,6 +12,55 @@
 #define false 0
 #endif
 
+#define PSF1_MAGIC0 0x36
+#define PSF1_MAGIC1 0x04
+
+
+typedef struct {
+	void* BaseAddress;
+	size_t BufferSize;
+	unsigned int Width;
+	unsigned int Height;
+	unsigned int PixelsPerScanline;
+} FrameBuffer;
+
+
+typedef struct {
+	unsigned char magic[2];
+	unsigned char mode;
+	unsigned char charsize;
+} PSF1_HEADER;
+
+
+typedef struct {
+	PSF1_HEADER* psf1_Header;
+	void* glyphBuffer;
+} PSF1_FONT;
+
+FrameBuffer frameBuffer;
+
+
+FrameBuffer* InitializeGOP() {
+	EFI_GUID gopGuid = EFI_GRAPHICS_OUTPUT_PROTOCOL_GUID;
+	EFI_GRAPHICS_OUTPUT_PROTOCOL* gop;
+	EFI_STATUS status;
+
+	status = uefi_call_wrapper(BS->LocateProtocol, 3, &gopGuid, NULL, (void**)&gop);
+	if (EFI_ERROR(status)) {
+		Print(L"Unable to locate GOP\n\r");
+		return NULL;
+	}
+	Print(L"GOP located\n\r");
+
+	frameBuffer.BaseAddress = (void*)gop->Mode->FrameBufferBase;
+	frameBuffer.BufferSize = gop->Mode->FrameBufferSize;
+	frameBuffer.Width = gop->Mode->Info->HorizontalResolution;
+	frameBuffer.Height = gop->Mode->Info->VerticalResolution;
+	frameBuffer.PixelsPerScanline = gop->Mode->Info->PixelsPerScanLine;
+
+	return &frameBuffer;
+}
+
 
 EFI_FILE* LoadFile (EFI_FILE* Directory, CHAR16* Path, EFI_HANDLE ImageHandle, EFI_SYSTEM_TABLE* SystemTable) {
 	EFI_FILE* LoadedFile;
@@ -31,6 +80,36 @@ EFI_FILE* LoadFile (EFI_FILE* Directory, CHAR16* Path, EFI_HANDLE ImageHandle, E
 	}
 	return LoadedFile;
 
+}
+
+
+PSF1_FONT* LoadPSF1Font(EFI_FILE* Directory, CHAR16* Path, EFI_HANDLE ImageHandle, EFI_SYSTEM_TABLE* SystemTable) {
+	EFI_FILE* font = LoadFile(Directory, Path, ImageHandle, SystemTable);
+	if (font == NULL) return NULL;
+
+	PSF1_HEADER* fontHeader;
+	SystemTable->BootServices->AllocatePool(EfiLoaderData, sizeof(PSF1_FONT), (void**)&fontHeader);
+	UINTN size = sizeof(PSF1_HEADER);
+	font->Read(font, &size, fontHeader);
+
+	if (fontHeader->magic[0] != PSF1_MAGIC0 || fontHeader->magic[1] != PSF1_MAGIC1) return NULL;
+
+	UINTN glyphBufferSize = fontHeader->charsize * 256;
+	if (fontHeader->mode == 1) glyphBufferSize = fontHeader->charsize * 512; // 512 glyph mode
+
+	void* glyphBuffer;
+	{
+		font->SetPosition(font, sizeof(PSF1_HEADER));
+		SystemTable->BootServices->AllocatePool(EfiLoaderData, glyphBufferSize, (void**)&glyphBuffer);
+		font->Read(font, &glyphBufferSize, glyphBuffer);
+	}
+
+	PSF1_FONT* finishedFont;
+	SystemTable->BootServices->AllocatePool(EfiLoaderData, sizeof(PSF1_FONT), (void**)&finishedFont); 
+	finishedFont->psf1_Header = fontHeader;
+	finishedFont->glyphBuffer = glyphBuffer;
+	
+	return finishedFont;
 }
 
 
@@ -109,8 +188,23 @@ EFI_STATUS efi_main (EFI_HANDLE ImageHandle, EFI_SYSTEM_TABLE *SystemTable) {
 
 	Print(L"Kernel loaded successfully\n\r");
 
-	int (*KernelStart)() = ((__attribute__((sysv_abi)) int (*)() ) header.e_entry);
-	Print(L"%d\n\r", KernelStart());
+	void (*KernelStart)() = 
+		((__attribute__((sysv_abi)) void (*)(FrameBuffer*, PSF1_FONT*) ) header.e_entry);
+	
+	PSF1_FONT* newFont= (LoadPSF1Font(NULL, L"zap-light16.psf", ImageHandle, SystemTable));	
+	if (newFont == NULL) {
+		Print(L"Font not found.");
+		while (true);
+	} else Print(L"Font found. char size = %d\n\r", newFont->psf1_Header->charsize);
+
+	FrameBuffer* newBuffer = InitializeGOP();
+
+	Print(
+		L"GOP:\n\r\tBase:		0x%x\n\r\tSize:		0x%x\n\r\tWidth:		%d\n\r\tHeight:		%d\n\r\tPPS:		%d\n\r", 
+		newBuffer->BaseAddress, newBuffer->BufferSize, newBuffer->Width, newBuffer->Height, newBuffer->PixelsPerScanline
+	);
+
+	KernelStart(newBuffer, newFont);
 
 	return EFI_SUCCESS; // Exit the UEFI application
 }
